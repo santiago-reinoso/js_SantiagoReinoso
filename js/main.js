a0ce9567c2f9f1afd98196388dc9241864d56bfb
reinoso santiago <reinososantiago43@gmail.com>
2024-09-16 00:10:56 -0300

    2da entrega

--- js/main.js
+++ js/main.js
@@ -1,71 +1,44 @@
-const zapatillas =[
-  {
-    id:1,
-    nombre:"yeezy 700",
-    precio:500,
-  },
-  {
-    id:2,
-    nombre:"forum low",
-    precio:430,
-  },
-  {
-    id:3,
-    nombre:"gazelle",
-    precio:200,
-  },
-  {
-    id:4,
-    nombre:"bad bunny",
-    precio:740,
-  },
-  {
-    id:5,
-    nombre:"jordab 4",
-    precio:545,
-  },
-  {
-    id:6,
-    nombre:"jordan 3",
-    precio:334,
-  },
-  {
-    id:7,
-    nombre:"jordan 5",
-    precio:670,
-  },
-  {
-    id:8,
-    nombre:"jordan 11",
-    precio:780,
-  },
-];
+const containerCards = document.getElementById('container-card');
+const selectProducts = document.getElementById('select-products');
 
-let cartrProducts = []
-let productsContainer = document.getElementById("products-container");
+window.addEventListener('load', listSelect);
+selectProducts.addEventListener('change', rendercards);
 
-function renderproductos(productsarray){
-  productsarray.forEach(producto => {
-    const card = document.createElement("div")
-    card.innerHTML = `<h3>${producto.nombre}</h3>
-                      <h4>${producto.precio}</h4>
-                      <button class="agregarproducto" id="${producto.id}">agregar a carrito</button>`
-
-                      productsContainer.appendChild(card)
-      })
-       addtocartbutton()
+function rendercards(){
+    zapatillas.map (zapatilla =>{zapatilla.producto === selectProducts.value ? createCards(fruit): null})
 }
-renderproductos(zapatillas)
 
-function addtocartbutton() {
-  addbutton = document.querySelectorAll(".agregarproducto")
-  addbutton.forEach(button => {
-    button.onclick = (e) => {
-      const productId = e.currentTarget.id
-      const selectedProduct = zapatillas.find(producto => producto.id == productId)
-       cartrProducts.push(selectedProduct)
-       console.log(cartrProducts)
-localStorage.setItem("cartproducts",JSON.stringify(cartrProducts))
-    }
-  })
+function listSelect() {
+    zapatillas.map(zapatilla => {
+        const option = document.createElement('option');
+        option.value = zapatilla.producto;
+        option.textContent = zapatilla.producto;
+        selectProducts.appendChild(option);
+    });
+}
+
+function createCards(zapatillas) {
+    const {producto, image, id, precio} = zapatillas;
+    const card = document.createElement('div');
+    card.classList.add('card-prodcut');
+    const imgcard = document.createElement('img')
+    imgcard.setAttribute('src',image);
+    imgcard.setAttribute('alt',`${id}-${producto}`);
+    imgcard.classList.add('img-product');
+    const namecard = document.createElement('p');
+    namecard.textContent = producto;
+    namecard.classList.add('name-product');
+    const precioCard = document.createElement('p');
+    precioCard.classList.add('precio-producto');
+    precioCard.textContent = "add to cart";
+   const btnAdd = document.createElement('button');
+   btnAdd.setAttribute('id',id);
+   btnAdd.classList.add('btn-add');
+   btnAdd.textContent = "Add to cart";
+    card.appendChild(imgcard);   
+    card.appendChild(namecard);
+    card.appendChild(precioCard);
+    card.appendChild(btnAdd);
+
+    containerCards.appendChild(card);
 }
